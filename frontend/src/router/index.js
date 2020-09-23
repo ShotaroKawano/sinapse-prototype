9ab453a38f136d40873838784b64978c5d47a467
ShotaroKawano <shotaro.kawano009@gmail.com>
2020-09-24 02:25:43 +0900

    Add: d3とaxiosの追加/eslintのoff項目追加

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import PlayScreen from '../../views/PlayScreen.vue'
+// import PlayScreen from '../../views/PlayScreen.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'PlayScreen',
-      component: PlayScreen
-    }
-  ]
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'PlayScreen',
+  //     component: PlayScreen
+  //   }
+  // ]
 })
