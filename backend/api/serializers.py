983367cfcc3692272c4b0f06f19a78d5e11bec68
sekiguchikeita <exchange.0219@gmail.com>
2020-09-28 19:32:29 +0900

    add : filterの追加

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,68 @@
 from rest_framework import serializers
+# from django_filters import rest_framework as filters 
 from backend.api.models import Like
+from backend.api.models import Comment
+from backend.api.models import Board
+from backend.api.models import Board_Tag
+from backend.api.models import Tag
+from backend.api.models import Card
+from backend.api.models import Arrow
+
+
+#今後dbに合わせて加工必要
 
 
 class LikeSerializer(serializers.ModelSerializer):
      class Meta:
           model = Like
           fields = ('created_at')
+
+
+# class LikeFilter(filters.FilterSet):
+# # フィルタの定義
+# created = filters.DateTimeFilter(lookup_expr='gt')
+# class Meta:
+#      model = Like
+#      # フィルタを列挙する。
+#      # デフォルトの検索方法でいいなら、モデルフィールド名のフィルタを直接定義できる。
+#      fields = ['created_at']
+
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Comment
+          fields = ('id','content','created_at','updated_at')
+
+class BoardSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Board
+          fields = ('id','title','description','thunbnail','url_tail','isPublished','created_at','updated_at')
+
+class Board_TagSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Board_Tag
+          fields = ('id')
+
+class TagSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Tag
+          fields = ('id','name')
+
+class CardSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Card
+          fields = ('id','url','title','summary','thumbnail','positionX','PositionY','created_at','updated_at')
+
+class ArrowSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Card
+          fields = ('id','from_card_id','to_card_id','label','arrow_type_id','created_at','updated_at')
+
+class Arrow_TypeSerializer(serializers.ModelSerializer):
+     class Meta:
+          model = Card
+          fields = ('id','type')
+
+
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,13 +2,48 @@ from django.conf.urls import include, url
 from rest_framework import routers
 # from .views import LikeViewSet, index
 from .views import LikeViewSet
+from .views import CommentViewSet
+from .views import BoardViewSet
+from .views import Board_TagViewSet
+from .views import TagViewSet
+from .views import CardViewSet
+from .views import ArrowViewSet
+from .views import Arrow_TypeViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'like', LikeViewSet)
+router.register(r'comment', CommentViewSet)
+router.register(r'board', BoardViewSet)
+router.register(r'board_tag', Board_TagViewSet)
+router.register(r'Tag', TagViewSet)
+router.register(r'card', CardViewSet)
+
 
 urlpatterns = [
     # qiita/api/stock/
+########################################
     url(r'like/', include(router.urls)),
     url(r'like/', LikeViewSet),
+########################################
+    url(r'comment/', include(router.urls)),
+    url(r'like/', CommentViewSet),
+########################################
+    url(r'board/', include(router.urls)),
+    url(r'board/', BoardViewSet),
+########################################
+    url(r'board_tag/', include(router.urls)),
+    url(r'board_tag/', Board_TagViewSet),
+########################################
+    url(r'tag/', include(router.urls)),
+    url(r'tag/', TagViewSet),
+########################################
+    url(r'card/', include(router.urls)),
+    url(r'card/', CardViewSet),   
+########################################
+    url(r'arrow/', include(router.urls)),
+    url(r'arrow/', ArrowViewSet),   
+########################################
+    url(r'arrow_type/', include(router.urls)),
+    url(r'arrow_type/', Arrow_TypeViewSet),   
 ]
