aa7782e71ee4469514cdd9bedff3c05e16b734a3
poly-tope <9538.jin@gmail.com>
2020-10-02 10:44:27 +0900

    update: this.$route.query.qで検索ワードを抽出する

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import BoardsHome from './components/BoardsHome'
-import Board from './components/Board'
+import Vue from "vue";
+import Router from "vue-router";
+import BoardsHome from "./components/BoardsHome";
+import Board from "./components/Board";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
-    { path: '/', component: BoardsHome },
-    { path: '/board', component: Board }
+    { path: "/", component: BoardsHome },
+    { path: "/board", component: Board },
+    { path: "/search", component: BoardsHome }
   ]
-})
+});
