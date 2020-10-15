93bc88d15561e93f7ccdfaa804d332ab1fd16c69
ShotaroKawano <shotaro.kawano009@gmail.com>
2020-10-15 13:57:15 +0900

    Fix: passwordを変更

--- backend/settings.py
+++ backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sinapseprototype',
         'USER': 'root',
-        'PASSWORD': 'pass',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
