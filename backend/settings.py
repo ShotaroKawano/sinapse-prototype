5455dda5bb07065199f949c342b5424c7f4935dc
ShotaroKawano <shotaro.kawano009@gmail.com>
2020-10-15 14:03:35 +0900

    Fix: herokuDBの設定を微修正

--- backend/settings.py
+++ backend/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sinapseprototype',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sinapseprototype',
+#         'USER': 'root',
+#         'PASSWORD': 'pass',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
